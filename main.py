95339db9e65d12ffd2dca9ba9d74342effc92829
Joseph Zica <josephalmedazica@gmail.com>
2023-10-05 19:41:04 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title('Suporte a denúncias :red[Tinder]')
+
+st.text('Informe seus dados e clique em enviar para prosseguirmos com o atendimento.')
+
+title = st.text_input('Nome completo')
+
+phone = st.text_input('Telefone')
+
+cpf = st.text_input('CPF')
